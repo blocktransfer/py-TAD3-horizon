9dc0ec1b900b89c57cc3cc76dbf422bf4a9f5341
John Wooten <jwooten31@gatech.edu>
2021-06-24 14:24:28 -0400

    compartmentalize logic; add country code

--- issuers/api-merge-logic.py
+++ issuers/api-merge-logic.py
@@ -1,70 +1,84 @@
 import requests
 from pprint import pprint
 
-queryAsset = 'StellarMart'
-restrictedShares = 69420
-limit = '200'
+searchLimitMax200 = '200'
 horizonInstance = 'horizon.stellar.org'
-BTIssuer = 'GD3VPKNLTLBEKRY56AQCRJ5JN426BGQEPE6OIX3DDTSEEHQRYIHIUGUM'
-requestAddress = 'https://' + horizonInstance + '/accounts?asset=' + queryAsset + ':' + BTIssuer + '&limit=' + limit
+BTissuerAddress = 'GD3VPKNLTLBEKRY56AQCRJ5JN426BGQEPE6OIX3DDTSEEHQRYIHIUGUM'
 
-accountBalancesStellar = []
-while(True):
+# With "formattingStockTestSheet.csv", use "StellarMart" as queryAsset and any numRestrictedShares
+def main(queryAsset, numRestrictedShares, PIIcsvSpreadsheetFileName):
+  accountBalancesFromStellar = getAccountBalancesFromStellar(queryAsset)
+  totalOutstandingShares = getTotalOutstandingShares(queryAsset, numRestrictedShares)
+  returnReport = mergeBlockchainRecordsWithPII(PIIcsvSpreadsheetFileName, totalOutstandingShares, accountBalancesFromStellar)
+  return returnReport
+
+def getAccountBalancesFromStellar(queryAsset):
+  accountBalancesFromStellar = []
+  requestAddress = 'https://' + horizonInstance + '/accounts?asset=' + queryAsset + ':' + BTissuerAddress + '&limit=' + searchLimitMax200
   r = requests.get(requestAddress)
   data = r.json()
   blockchainRecords = data['_embedded']['records']
-  if blockchainRecords == []:
-    break
-  for eachAccount in blockchainRecords:
-    accountAddress = eachAccount['id']
-    for balances in eachAccount['balances']:
-      if balances['asset_type'] != 'native' and balances['asset_code'] == queryAsset:
-        accountBalance = balances['balance']
+  while(blockchainRecords != []):
+    for eachAccount in blockchainRecords:
+      accountAddress = eachAccount['id']
+      for balances in eachAccount['balances']:
+        if balances['asset_type'] != 'native' and balances['asset_code'] == queryAsset:
+          accountBalance = balances['balance']
+          break
+      accountBalancesFromStellar.append((accountAddress, accountBalance))
+    # Go to next cursor
+    requestAddress = data['_links']['next']['href'].replace('%3A', ':')
+    r = requests.get(requestAddress)
+    data = r.json()
+    blockchainRecords = data['_embedded']['records']
+  return accountBalancesFromStellar
+
+def getTotalOutstandingShares(queryAsset, numRestrictedShares):
+  requestAddress = 'https://' + horizonInstance + '/ledgers?asset=' + queryAsset + ':' + BTissuerAddress
+  r = requests.get(requestAddress)
+  data = r.json()
+  numUnrestrictedShares = data['_embedded']['records'][len(data['_embedded']['records']) - 1]['total_coins']
+  totalOutstandingShares = float(numRestrictedShares) + float(numUnrestrictedShares)
+  return totalOutstandingShares
+
+def mergeBlockchainRecordsWithPII(PIIcsvSpreadsheetFileName, totalOutstandingShares, accountBalancesStellar):
+  inFile = open(PIIcsvSpreadsheetFileName)
+  readFile = inFile.read()
+  readFile = readFile.strip()
+  readFile = readFile.split('\n')
+  inFile.close()
+
+  linkedAccounts = []
+  skipHeaderYet = False
+  for lines in readFile:
+    if not skipHeaderYet:
+      skipHeaderYet = True
+      continue
+    lines = lines.split(',')
+    addressStellar = lines[0]
+    nameInstitution = lines[1]
+    nameFirst = lines[2]
+    nameMiddle = lines[3]
+    nameLast = lines[4]
+    nameSuffix = lines[5]
+    addressPhysicalLine1 = lines[6]
+    addressPhysicalLine2 = lines[7]
+    addressCity = lines[8]
+    addressStateProvince = lines[9]
+    addressAreaCode = lines[10]
+    country = lines[11]
+    SSN = lines[12]
+    EIN = lines[13]
+    TIN = lines[14]
+    driversLicenseNumber = lines[15]
+    passportNumber = lines[16]
+    otherID = lines[17]
+    onboardedDate = lines[18]
+    otherKYCinternal = lines[19]
+    email = lines[20]
+    for account,balance in accountBalancesStellar:
+      if account == addressStellar:
         break
-    accountBalancesStellar.append((accountAddress, accountBalance))
-  requestAddress = data['_links']['next']['href'].replace('%3A', ':')
-
-requestAddress = 'https://' + horizonInstance + '/ledgers?asset=' + queryAsset + ':' + BTIssuer
-r = requests.get(requestAddress)
-data = r.json()
-unrestrictedShares = data['_embedded']['records'][len(data['_embedded']['records']) - 1]['total_coins']
-totalOutstanding = float(restrictedShares) + float(unrestrictedShares)
-
-PIIcsvName = "insecureStockTestSheet.csv"
-inFile = open(PIIcsvName)
-readFile = inFile.read()
-readFile = readFile.strip()
-readFile = readFile.split('\n')
-inFile.close()
-
-linkedAccounts = []
-for lines in readFile:
-  lines = lines.split(',')
-  addressStellar = lines[0]
-  if addressStellar == 'Blockchain Address':
-    continue
-  nameInstitution = lines[1]
-  nameFirst = lines[2]
-  nameMiddle = lines[3]
-  nameLast = lines[4]
-  nameSuffix = lines[5]
-  addressPhysicalLine1 = lines[6]
-  addressPhysicalLine2 = lines[7]
-  addressCity = lines[8]
-  addressStateProvince = lines[9]
-  addressAreaCode = lines[10]
-  SSN = lines[11]
-  EIN = lines[12]
-  TIN = lines[13]
-  driversLicenseNumber = lines[14]
-  passportNumber = lines[15]
-  otherID = lines[16]
-  onboardedDate = lines[17]
-  otherKYCinternal = lines[18]
-  email = lines[19]
-  for account,balance in accountBalancesStellar:
-    if account == addressStellar:
-      break
-  balanceAsPercentOfOutstandingShares = 100 * float(balance) / totalOutstanding
-  linkedAccounts.append((account, balance, balanceAsPercentOfOutstandingShares, nameInstitution, nameFirst, nameMiddle, nameLast, nameSuffix, addressPhysicalLine1, addressPhysicalLine2, addressCity, addressStateProvince, addressAreaCode, SSN, EIN, TIN, driversLicenseNumber, passportNumber, otherID, onboardedDate, otherKYCinternal, email))
-  
+    balanceAsPercentOfOutstandingShares = 100 * float(balance) / totalOutstandingShares
+    linkedAccounts.append((account, balance, balanceAsPercentOfOutstandingShares, nameInstitution, nameFirst, nameMiddle, nameLast, nameSuffix, addressPhysicalLine1, addressPhysicalLine2, addressCity, addressStateProvince, addressAreaCode, SSN, EIN, TIN, driversLicenseNumber, passportNumber, otherID, onboardedDate, otherKYCinternal, email))
+  return linkedAccounts
