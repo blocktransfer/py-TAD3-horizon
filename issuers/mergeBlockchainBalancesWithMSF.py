ab16753392cbb25205e519c481c514fc865e6374
John Wooten <jwooten31@gatech.edu>
2021-07-10 11:54:32 -0400

    dict implemented; writing next

--- issuers/mergeBlockchainBalancesWithMSF.py
+++ issuers/mergeBlockchainBalancesWithMSF.py
@@ -4,15 +4,15 @@ searchLimitMax200 = '200'
 horizonInstance = 'horizon.stellar.org'
 BTissuerAddress = 'GD3VPKNLTLBEKRY56AQCRJ5JN426BGQEPE6OIX3DDTSEEHQRYIHIUGUM'
 
-# With "exampleMasterSecurityholderFile.csv", use "StellarMart" as queryAsset and any numRestrictedShares
+# With "VeryRealStockIncMSF.csv", use "StellarMart" as queryAsset and any numRestrictedShares
 def getMergedReportForAssetWithNumRestrictedSharesUsingMSF(queryAsset, numRestrictedShares, MSF):
-  accountBalancesFromStellar = getAccountBalancesFromStellar(queryAsset)
+  StellarBlockchainBalances = getStellarBlockchainBalances(queryAsset)
   totalOutstandingShares = getTotalOutstandingShares(queryAsset, numRestrictedShares)
-  isSuccessful = mergeBlockchainRecordsWithMSF(MSF, totalOutstandingShares, accountBalancesFromStellar)
-  return isSuccessful
+  didSucceed = mergeBlockchainRecordsWithMSF(MSF, totalOutstandingShares, StellarBlockchainBalances)
+  return didSucceed
 
-def getAccountBalancesFromStellar(queryAsset):
-  accountBalancesFromStellar = []
+def getStellarBlockchainBalances(queryAsset):
+  StellarBlockchainBalances = {}
   requestAddress = 'https://' + horizonInstance + '/accounts?asset=' + queryAsset + ':' + BTissuerAddress + '&limit=' + searchLimitMax200
   r = requests.get(requestAddress)
   data = r.json()
@@ -24,13 +24,13 @@ def getAccountBalancesFromStellar(queryAsset):
         if balances['asset_type'] != 'native' and balances['asset_code'] == queryAsset:
           accountBalance = float(balances['balance'])
           break
-      accountBalancesFromStellar.append((accountAddress, accountBalance))
+      StellarBlockchainBalances[accountAddress] = accountBalance
     # Go to next cursor
     requestAddress = data['_links']['next']['href'].replace('%3A', ':')
     r = requests.get(requestAddress)
     data = r.json()
     blockchainRecords = data['_embedded']['records']
-  return accountBalancesFromStellar
+  return StellarBlockchainBalances
 
 def getTotalOutstandingShares(queryAsset, numRestrictedShares):
   requestAddress = 'https://' + horizonInstance + '/ledgers?asset=' + queryAsset + ':' + BTissuerAddress
@@ -40,38 +40,37 @@ def getTotalOutstandingShares(queryAsset, numRestrictedShares):
   totalOutstandingShares = numRestrictedShares + numUnrestrictedShares
   return totalOutstandingShares
 
-def mergeBlockchainRecordsWithMSF(MSF, totalOutstandingShares, accountBalancesStellar):
+def mergeBlockchainRecordsWithMSF(MSF, totalOutstandingShares, StellarBlockchainBalances):
   inFile = open(MSF)
   readFile = inFile.read()
-
-  issuer = inFile.readline()
-
   readFile = readFile.strip()
   readFile = readFile.split('\n')
   inFile.close()
-  linkedAccounts = []
-  # rewrite first 2 headers
-
-  try:
-    issuer = issuer.split(',')
-    delimiter = ','
-  except:
-    delimiter = '\t'
-
-  #
-  for lines in readFile[2:]:
-    lines = lines.split(delimiter)
-    securityholderStellarAddress = lines[0]
-    sharesNotYetClaimedOnStellar = lines[1]
-    for account,balance in accountBalancesStellar:
-      if account == securityholderStellarAddress:
-        balanceAsPercentOfOutstandingShares = 100 * float(balance) / totalOutstandingShares
-        lines[0] = balance + sharesNotYetClaimedOnStellar
-        break
-  writeFile = open(MSF, 'w')
-  writeFile = writeFile
-  writeFile.close()
-  return True
+  #linkedAccounts = []
   
+  writeCSV = []
+  
+  
+  for lines in readFile[1:]:
+    lines = lines.split(',')
+    sharesNotYetClaimedOnStellar = 0 if lines[1] == '' else float(lines[1])
+    try:
+        blockchainBalance = 0 if lines[0] == '' else StellarBlockchainBalances[lines[0]]
+    except KeyError:
+        # This address is no longer a securityholder per removed trustline. Prune from merged MSF
+        continue
+    balanceAsPercentOfOutstandingShares = 100 * float(blockchainBalance) / totalOutstandingShares
+    lines[0] = blockchainBalance + sharesNotYetClaimedOnStellar
+    lines[1] = balanceAsPercentOfOutstandingShares
+    
+    writeCSV.append(lines)
+  #mergedMSF = 'merged' + MSF + 'AsOf' + date.get()
+  #writeFile = open(mergedMSF, 'a')
+  #writeFile = writeFile
+  #writeFile.close()
+  return True
+
+
+
 
 getMergedReportForAssetWithNumRestrictedSharesUsingMSF("StellarMart", 10000, "testingMasterSecurityholderFile.csv")
