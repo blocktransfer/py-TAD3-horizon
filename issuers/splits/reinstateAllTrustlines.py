e404289ac90e78674be44efa5117651816e1087d
John Wooten <jwooten31@gatech.edu>
2021-08-29 11:04:17 -0400

    baselines pre-CAP 42
    may -> SEP if burn works for accounts under 1 [asset]

---
+++ issuers/splits/reinstateAllTrustlines.py
@@ -0,0 +1,12 @@
+import requests
+from datetime import datetime
+
+searchLimitMax200 = '200'
+HorizonInstance = 'horizon.stellar.org'
+BTissuerAddress = 'GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7'
+
+# testing: getMergedReportForAssetWithNumRestrictedSharesUsingMSF("StellarMart", 10000, "VeryRealStockIncMSF.csv")
+# with BTissuerAddress = 'GD3VPKNLTLBEKRY56AQCRJ5JN426BGQEPE6OIX3DDTSEEHQRYIHIUGUM')
+def freezeAllTrustlines(queryAsset):
+  # Full privilege
+  return didSucceed
