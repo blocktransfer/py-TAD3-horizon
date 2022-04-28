82c75c27c62581d412df328bcc9fce765b922fe0
John Wooten <jwooten31@gatech.edu>
2022-04-28 08:07:30 -0400

    Update bulkApproveTrustlines.py

--- securityholders/onboarding/bulkApproveTrustlines.py
+++ securityholders/onboarding/bulkApproveTrustlines.py
@@ -2,15 +2,25 @@ import stellar ...
 import requests
 import json
 
+SecretKey = "ABCD..." # Admin temporary 1-weight signers... execute on offline airgapped sys
+
 identityMappingCSV = "" # todo: make a style for a master identity ledger... store on offline airgapps sys with weekly? updates and sole physical backup monthly? with secure custodians (split btwn with partial images? - registered mail encrypted drives?) and then wipe Persona ea. week? on a 2-mo delayed basis? 
 # that might be a bit much, and we could probably just use an authenticated sftp channel or put in Storj? 
 HorizonInstance = "horizon.stellar.org"
-SecretKey = "ABCD..." # Admin temporary 1-weight signers... execute on offline airgapped sys
 minFeePerOp = .00001 # is there a get call for 100 stoops? 
 
 def bulkApproveTruslines():
   r = "https://" + HorizonInstance + "..."
   data = r.json()
+  bulkTxnXDR = ""
+  
+  KYC_known = fopen(identityMappingCSV) ... 
+  KYC_known.readline()
+  i = -1
+  while(KYC_known and i++):
+    inputCSV = KYC_known.readline()
+    all_verified_addresses[i] = inputCSV.split(',')[0]
+  
   
   ...
   
@@ -19,6 +29,12 @@ def bulkApproveTruslines():
   while(shareholder):
     do...
     
+    
+    public_address = shareholder[...]
+    if(public_address in all_verified_addresses):
+      #make trustlines approval
+      trustline_approval = stellar.AuthorizeTrust(public_address, ...)
+      bulkTxnXDR.append(trustline_approval) #does bulkTxnXDR need to be a list or what? 
     data = r.json()
     shareholder = data[...]
   
