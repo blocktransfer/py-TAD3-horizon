15a9958a30bf16abd2016d922f3f66a5012ea45e
John Wooten <jwooten31@gatech.edu>
2022-04-28 08:20:41 -0400

    Update bulkApproveTrustlines.py
    This actually approves the last thing they want as ordered in the XDR, but that's fine and functionally equivalent

--- securityholders/onboarding/bulkApproveTrustlines.py
+++ securityholders/onboarding/bulkApproveTrustlines.py
@@ -21,20 +21,20 @@ def bulkApproveTruslines():
     inputCSV = KYC_known.readline()
     all_verified_addresses[i] = inputCSV.split(',')[0]
   
+  trustlinesToPotentiallyApproveWithAsset = {}
   pendingTrustline = data[...]
   while(pendingTrustline): # search over all pending trustlines and get pub key 
+    potentialAddress = pendingTrustline[...]
+    potentialAsset = pendingTrustline[...]
+    trustlinesToPotentiallyApproveWithAsset[potentialAddress] = potentialAsset
     
-    
-    
-    # add their address and asset in a dictionary 
-    
-    # just approve basically the first thing they want...
-    # we can run this multiple times if they are waiting for approval for a bunch of different assets, since dict. mapping becomes easier this way. 
-    # In scaled production, this script should basically run every couple minutes? with better key provisioning. But @ the start ppl prob just want their one stock or something 
-    # We could make this an arr but I just don't think that's good for speed and we want fast trustline approvals (think 9:30am adding a trustline for a fat open) 
-    # Further, this incentivizes people to maintain trustlines in case they need them quickly later, which Issuer can happily sponsor, which increases overall number of network connections and benefits all participants 
+    r = "https://" + HorizonInstance + "..." # this might need to be newR 
+    data = r.json() # ditto 
+    shareholder = data[...]
+
   ...
   
+  verifiedTrustlinesToApproveWithAsset = {}
   # search over approved securitholder list + address mappping
   shareholder = data[...]
   while(shareholder):
