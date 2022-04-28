99dc8b219d0e3473dfcccdb1fe75556bf7b6dffc
John Wooten <jwooten31@gatech.edu>
2022-04-28 08:45:21 -0400

    Continued

--- securityholders/onboarding/bulkApprovePendingTruslines.py
+++ securityholders/onboarding/bulkApprovePendingTruslines.py
@@ -9,34 +9,41 @@ identityMappingCSV = "" # todo: make a style for a master identity ledger... sto
 HorizonInstance = "horizon.stellar.org"
 minFeePerOp = .00001 # is there a get call for 100 stoops? in case minBaseFee changes one day 
 maxNumOpsPerTxn = 100
+BT_issuer = "GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7" # check for consistency for this field against other scripts
 
 def getKnownAddressesFromIdentityMappingCSV(inputCSV):
   KYC_known = fopen(inputCSV)
   KYC_known.readline()
+  all_verified_addresses[] = ""
   i = -1
   while(KYC_known and i++):
     KYC_known = KYC_known.readline()
     all_verified_addresses[i] = inputCSV.split(',')[0]
+  return all_verified_addresses
+
+def getAllPendingTrustlinesWithAsset():
+  r = "https://" + HorizonInstance + "..." + BT_issuer + "..."
+  data = r.json()
+  
+  allPendingTrustlines = {}
+  pendingTrustline = data[...]
+  while(pendingTrustline):
+    potentialAddress = pendingTrustline[...]
+    potentialAsset = pendingTrustline[...]
+    allPendingTrustlines[potentialAddress] = potentialAsset
+    r = "https://" + HorizonInstance + "..." + BT_issuer + "..." -> next
+    data = r.json()
+    pendingTrustline = data[...]
+  return allPendingTrustlines
 
 def bulkApproveOutstandingTruslines():
-  r = "https://" + HorizonInstance + "..."
-  data = r.json()
+  
   bulkTxnXDR = ""
   
   KYC_known = getKnownAddressesFromIdentityMappingCSV(identityMappingCSV)
+  pendingTrustlinesAndAsset = getAllPendingTrustlinesWithAsset()
   
-  trustlinesToPotentiallyApproveWithAsset = {}
-  pendingTrustline = data[...]
-  while(pendingTrustline): # search over all pending trustlines and get pub key 
-    potentialAddress = pendingTrustline[...]
-    potentialAsset = pendingTrustline[...]
-    trustlinesToPotentiallyApproveWithAsset[potentialAddress] = potentialAsset
-    
-    r = "https://" + HorizonInstance + "..." # this might need to be newR 
-    data = r.json() # ditto 
-    pendingTrustline = data[...]
-
-  ...
+  
   
   verifiedTrustlinesToApproveWithAsset = {}
   # search over approved securitholder list + address mappping
