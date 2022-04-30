bd0601432ec5d737adaef8837eb0e7714dd0eda9
John Wooten <jwooten31@gatech.edu>
2022-04-30 12:51:21 -0400

    Update forwardSplit.py
    Need to make the XDR functions return arrays of txns to overcome 100 op limit

--- securityholders/splits/forwardSplit.py
+++ securityholders/splits/forwardSplit.py
@@ -1,14 +1,23 @@
-import requests
+from stellar_sdk import Asset, TransactionBuilder
 from datetime import datetime
+from fractions import Fraction
+from decimal import Decimal
+import requests
+import json
 
-searchLimitMax200 = '200'
-HorizonInstance = 'horizon.stellar.org'
-BTissuerAddress = 'GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7'
+HorizonInstance = "horizon.stellar.org"
+fallbackMinFeeInStroops = 100
+maxNumOpsPerTxn = 100
+BT_ISSUER = "GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7"
 
-# testing: getMergedReportForAssetWithNumRestrictedSharesUsingMSF("StellarMart", 10000, "VeryRealStockIncMSF.csv")
-# with BTissuerAddress = 'GD3VPKNLTLBEKRY56AQCRJ5JN426BGQEPE6OIX3DDTSEEHQRYIHIUGUM')
-def forwardSplit(queryAsset, numerator, denominator):
-  # FREEZE BEFOREHAND!
+def forwardSplit(asset, numerator, denominator, preSplitBalancesCSV):
+  balances = fopen(preSplitBalancesCSV, "r")
+  splitRatio = Fraction(numerator, denominator)
   # Get all account balances
+  
   # Create issuance ops per split ratio
-  return didSucceed
+  
+  
+  
+  
+  balances.close()
