9103fb18ee28e5097f730c9ef42c65b929344274
John Wooten <jwooten31@gatech.edu>
2022-04-30 18:31:53 -0400

    Update forwardSplit.py

--- securityholders/splits/forwardSplit.py
+++ securityholders/splits/forwardSplit.py
@@ -14,6 +14,7 @@ MAX_NUM_DECIMALS = "7"
 FALLBACK_MIN_FEE = 100
 MAX_NUM_TXN_OPS = 100
 BT_ISSUER = "GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7"
+BT_DISTRIBUTOR = "GAQKSRI4E5643UUUMJT4RWCZVLY25TBNZXDME4WLRIF5IPOLTLV7N4N6"
 
 postSplitFileName = "{} Post-Split Master Securityholder File.csv"
 
@@ -28,42 +29,39 @@ def grantMSFnewSplitSharesUnclaimedOnStellarInclRestricted(MSFpreSplitBalancesCS
   for shareholder in oldMSF[1:]: # Assume restricted entries are separate from unrestricted entries 
     if(shareholder[0] == ""):
       shareholder = shareholder.split(",")
-      shareholder[1] = Decimal(shareholder[1]) * numerator / denominator
+      sharesAfterSplit = Decimal(shareholder[1]) * numerator / denominator
+      shareholder[1] = str(sharesAfterSplit)
       newMSF.write(",".join(shareholder) + "\n")
   newMSF.close()
   return newMSF
 
 def grantNewSplitSharesFromBalancesClaimedOnStellar(StellarBlockchainBalances, queryAsset, numerator, denominator):
   server = Server(horizon_url= "https://" + HORIZON_INST)
-  issuer = server.load_account(account = BT_ISSUER)
+  distributor = server.load_account(account = BT_DISTRIBUTOR)
   try: 
     fee = server.fetch_base_fee()
   except: 
     fee = FALLBACK_MIN_FEE
-  
-  
   transactions[0] = TransactionBuilder(
-    source_account = issuer,
+    source_account = distributor,
     network_passphrase = Network.PUBLIC_NETWORK_PASSPHRASE,
-    base_fee = fee,
-  )
+    base_fee = fee, )
   reason = str(numerator) + "-for-" + str(denominator) + " forward stock split"
   i, idx = 0
   for address, balance in StellarBlockchainBalances:
+    sharesToPay = (balance * numerator / denominator) - balance
     transactions[idx].append_payment_op(
       destination = address,
       asset = Asset(queryAsset, BT_ISSUER),
-      amount = (balance * numerator / denominator) - balance
-    )
+      amount = ("{:." + MAX_NUM_DECIMALS + "f}").format(sharesToPay), )
     if(++i and i >= MAX_NUM_TXN_OPS):
       transactions[idx].add_text_memo(reason).set_timeout(7200).build()
       i = 0
       idx++
       transactions[idx] = TransactionBuilder(
-        source_account = issuer,
+        source_account = distributor,
         network_passphrase = Network.PUBLIC_NETWORK_PASSPHRASE,
-        base_fee = fee,
-      )
+        base_fee = fee, )
   transactions[idx].add_text_memo(reason).set_timeout(7200).build()
   return transactions
 
@@ -72,6 +70,9 @@ def exportSplitNewShareTransactions(txnXDRarr):
       output = open(datetime.now() + " forwardSplitPaymentXDR", "w")
       output.write(bulkTxnXDR)
       output.close()
+
+def generateFinalPostSplitMSF(outputMSF, MSFpreSplitBalancesCSV):
+  
 
 def forwardSplit(queryAsset, numerator, denominator, MSFpreSplitBalancesCSV):
   numerator = Decimal(numerator)
@@ -89,5 +90,5 @@ def forwardSplit(queryAsset, numerator, denominator, MSFpreSplitBalancesCSV):
   generateFinalPostSplitMSF(outputPostSplitMSFwithUnclaimedShareholdersOnly, MSFpreSplitBalancesCSV)
   
   newMSF = open(postSplitFileName.format(queryAsset), "a")
-  ("{:." + MAX_NUM_DECIMALS + "f}").format(shares)
+  
 
