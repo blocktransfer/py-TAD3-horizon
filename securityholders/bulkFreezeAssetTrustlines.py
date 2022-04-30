a3b5fd0e2749afcfb6f5b46b77019812ee1e3862
John Wooten <jwooten31@gatech.edu>
2022-04-30 11:22:00 -0400

    Splits

--- securityholders/bulkFreezeAssetTrustlines.py
+++ securityholders/bulkFreezeAssetTrustlines.py
@@ -1,8 +1,19 @@
 from stellar_sdk import TransactionBuilder
+from datetime import datetime
 import requests
 import json
 
-# Used to freeze all asset trustlines for stock splits or regulatory reasons
+searchLimitMax200 = '200'
+HorizonInstance = 'horizon.stellar.org'
+BT_ISSUER = 'GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7'
+
+def freezeAllTrustlines(queryAsset):
+  # INCLUDING CANCELLING ALL OUTSTANDING OFFERS
+  return didSucceed
 
 
-...
+
+
+
+
+def bulkFreezeAssetTrustlines
