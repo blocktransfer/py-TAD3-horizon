3342b258ee7bf32648d251e0608106cb986bdfbe
John Wooten <jwooten31@gatech.edu>
2022-05-01 17:14:49 -0400

    Update reverseSplit.py

--- securityholders/splits/reverseSplit.py
+++ securityholders/splits/reverseSplit.py
@@ -5,20 +5,13 @@ from globals import *
 postSplitFileName = "[BACKWARDS] {} Post-Split Master Securityholder File.csv"
 
 def reverseSplit(queryAsset, numerator, denominator):
-  # FREEZE BEFOREHAND!
-  # Get all account balances
+  
   # Create burn ops per split ratio
-  
-  test forwardSplit. ~Same functionality here (perhaps modularize grantMSFnewSplitSharesUnclaimedOnStellarInclRestricted since everything is the same except for sharesAfterSplit
-  
   transactions[idx].append_clawback_op(
       asset = Asset(queryAsset, BT_ISSUER),
       from = address,
-      amount = ("{:." + MAX_NUM_DECIMALS + "f}").format(sharesToClawback), )
-  
-  e.g. the export txns function could be modularized with a reason/name field etc
-  
-  
-  
-  
-  return didSucceed
+      amount = ("{:." + MAX_NUM_DECIMALS + "f}").format(sharesToClawback), )  
+
+
+
+copy/paste forward split 
