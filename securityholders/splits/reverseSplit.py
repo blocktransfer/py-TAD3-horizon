1d52464ac277eba28556aa81ee1e316ecd99d3db
John Wooten <jwooten31@gatech.edu>
2022-05-01 08:50:07 -0400

    Update reverseSplit.py
    perhaps overthinking this

--- securityholders/splits/reverseSplit.py
+++ securityholders/splits/reverseSplit.py
@@ -11,4 +11,17 @@ def reverseSplit(queryAsset, numerator, denominator):
   # FREEZE BEFOREHAND!
   # Get all account balances
   # Create burn ops per split ratio
+  
+  test forwardSplit. ~Same functionality here (perhaps modularize grantMSFnewSplitSharesUnclaimedOnStellarInclRestricted since everything is the same except for sharesAfterSplit
+  
+  transactions[idx].append_clawback_op(
+      asset = Asset(queryAsset, BT_ISSUER),
+      from = address,
+      amount = ("{:." + MAX_NUM_DECIMALS + "f}").format(sharesToClawback), )
+  
+  e.g. the export txns function could be modularized with a reason/name field etc
+  
+  
+  
+  
   return didSucceed
