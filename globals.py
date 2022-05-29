66a256e38b366dc095c7d47ddaf8e5e71dda1498
John Wooten <jwooten31@gatech.edu>
2022-05-29 15:30:18 -0400

    Update globals.py

--- globals.py
+++ globals.py
@@ -6,6 +6,7 @@ import requests, json
 
 BT_ISSUER = "GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7"
 BT_DISTRIBUTOR = "GAQKSRI4E5643UUUMJT4RWCZVLY25TBNZXDME4WLRIF5IPOLTLV7N4N6"
+SECRET = "SBTPLXTXJDMJOXFPYU2ANLZI2ARDPHFKPKK4MJFYVZVBLXYM5AIP3LPK"
 
 HORIZON_INST = "horizon.stellar.org"
 MAX_NUM_DECIMALS = "7"
@@ -14,7 +15,6 @@ MAX_SEARCH = "200"
 FALLBACK_MIN_FEE = 100
 MAX_NUM_TXN_OPS = 100
 
-SECRET = "SBTPLXTXJDMJOXFPYU2ANLZI2ARDPHFKPKK4MJFYVZVBLXYM5AIP3LPK"
 KYC_CSV_INST = "" # todo: make a style for a master identity ledger... store on offline airgapps sys with weekly? updates and sole physical backup monthly? with secure custodians (split btwn with partial images? - registered mail encrypted drives?) and then wipe Persona ea. week? on a 2-mo delayed basis? 
 # that might be a bit much, and we could probably just use an authenticated sftp channel or put in Storj? 
 
