de70493450361bd3f7bfef340f2c06c98de9a9a1
John Wooten <60260750+JFWooten4@users.noreply.github.com>
2022-07-10 14:43:49 -0400

    Update countProxyVotes.py
    need to create the data to test and likely another function for delegation

--- issuers/countProxyVotes.py
+++ issuers/countProxyVotes.py
@@ -10,12 +10,19 @@ def countProxyVotes(queryAsset, numVotingItems):
   votingFederationAddress = queryAsset + "*proxyvote.io"
   numUnrestrictedShares = getNumUnrestrictedShares(queryAsset)
   blockchainBalancesOnRecordDate = getBalancesOnRecordDate(queryAsset)
-  delegationHashmap = makeHashmapForDelegation()
   addressesMappedToMemos = getAddressesMappedToMemos(queryAsset, votingFederationAddress)
-  balancesMappedToMemos = replaceAddressesWithRecordDateBalances(queryAsset, recordDateInvestorBalancesCSV)
+  balancesMappedToMemos = replaceAddressesWithRecordDateBalances(addressesMappedToMemos, blockchainBalancesOnRecordDate)
+  voteResults = parseMemosToVotes(balancesMappedToMemos, numVotingItems)
   
+  print(numUnrestrictedShares)
+  print("---")
+  pprint(blockchainBalancesOnRecordDate)
+  print("---")
+  pprint(addressesMappedToMemos)
+  print("---")
+  pprint(balancesMappedToMemos)
 
-def getNumUnrestrictedShares(queryAsset): # TODO: change diction to numOutstandingSharesElidgibleToVote per 8-K review
+def getNumUnrestrictedShares(queryAsset): # TODO: change diction to numOutstandingSharesElidgibleToVote pending 8-K review
   requestAddr = "https://" + HORIZON_INST + "/assets?asset_code=" + queryAsset + "&asset_issuer=" + BT_ISSUER
   data = requests.get(requestAddr).json()
   return Decimal(data["_embedded"]["records"][0]["amount"]) # Assume no voting of restricted shares
@@ -38,8 +45,8 @@ def makeHashmapForDelegation():
   inFile.close()
   for lines in MICR[1:]:
     lines = lines.split(",")
-    SHA256ofPublicKey = hashlib.sha256(lines[0].encode())
-    delegationHashmap[SHA256ofPublicKey.hexdigest()] = lines[0]
+    SHA256ofPublicKey = hashlib.sha256(lines[0].encode()).hexdigest()
+    delegationHashmap[SHA256ofPublicKey] = lines[0]
   return delegationHashmap
 
 def getAddressesMappedToMemos(queryAsset, votingFederationAddress):
@@ -67,5 +74,54 @@ def getAddressesMappedToMemos(queryAsset, votingFederationAddress):
     paymentRecords = paymentData["_embedded"]["records"]
   return addressesMappedToMemos
 
-#queryAssetOnStellar = Asset(queryAsset, BT_ISSUER)
+def replaceAddressesWithRecordDateBalances(addressesMappedToMemos, blockchainBalancesOnRecordDate):
+  balancesMappedToMemos = {}
+  for addresses, balances in blockchainBalancesOnRecordDate.items():
+    try:
+      balancesMappedToMemos[balances] = addressesMappedToMemos[addresses]
+    except KeyError:
+      continue
+  return balancesMappedToMemos
+
+def parseMemosToVotes(balancesMappedToMemos, numVotingItems):
+  delegationHashmap = makeHashmapForDelegation()
+  delegeesPublicKeysMappedToSharesAllocated = {}
+  propositionYays = propositionNays = propositionAbstains = [0] * numVotingItems
+  numSharesVoted = 0
+  for numShares, memos in balancesMappedToMemos:
+    if(len(memo) > numVotingItems):
+      if(memo in delegationHashmap.keys()):
+        delegeesPublicKeysMappedToSharesAllocated[delegationHashmap[memo]] += numShares
+        # numSharesVoted += numShares
+    else:
+      numSharesVoted += numShares
+      voteList = [votes.upper() for votes in list(memo)]
+      try:
+        for i, votes in enumerate(voteList()):
+          match status:
+            case "Y":
+              propositionYays[i] += numShares
+            case "N":
+              propositionNays[i] += numShares
+            case "A":
+              propositionAbstains[i] += numShares
+            ###
+      except Exception:
+        print("Debug: Exception on vote parser with memo {}".format(memo))
+        continue
+  for delegees, sharesAllocated in delegeesPublicKeysMappedToSharesAllocated.items():
+    if(sharesAllocated):
+      sys.exit("todo: DELEGATION FUNCTIONALITY REQUIRED")
+  voteResults = []
+  numSharesVoted = Decimal(numSharesVoted)
+  oneH = Decimal(100)
+  for i in range(numVotingItems):
+    yays = propositionYays[i]
+    nays = propositionNays[i]
+    abstains = propositionAbstains[i]
+    voteResults.append((yays, nays, abstains))
+    voteResults.append((oneH*Decimal(yays)/numSharesVoted, oneH*Decimal(nays)/numSharesVoted, oneH*Decimal(abstains)/numSharesVoted))
+    
+  return voteResults
+
 countProxyVotes("StellarMart", 7)
