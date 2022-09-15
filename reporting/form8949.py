0da41396c99c112c29ec35eeb37c49fe2f7dfc7d
John Wooten <jwooten31@gatech.edu>
2022-09-15 04:49:32 -0400

    Update form8949.py

--- reporting/form8949.py
+++ reporting/form8949.py
@@ -14,16 +14,54 @@ taxYearStart = pandas.to_datetime(f"{lastYear}-01-01T00:00:00Z") # modify here f
 def getAllTxnsFromLastYear():
   # - fetch account
   requestAddr = f"https://{HORIZON_INST}/accounts/{publicKey}/trades?limit={MAX_SEARCH}"
+  print(requestAddr)
   data = requests.get(requestAddr).json()
   blockchainRecords = data["_embedded"]["records"]
-  a = 0
-  b = 0
+  b=9
   while(blockchainRecords != []):
-    print(requestAddr)
     for trades in blockchainRecords:
-      bis = trades["base_is_seller"]
-      if(bis):
-        a += 1
+      IDs = trades["id"].split("-")[0]
+      settlementTime = trades["ledger_close_time"]
+      baseAddr = trades["base_account": "GD2OUJ4QKAPESM2NVGREBZTLFJYMLPCGSUHZVRMTQMF5T34UODVHPRCY",
+      trades["base_amount": "129.9633530",
+      trades["base_asset_type": "credit_alphanum4",
+      trades["base_asset_code": "USDC",
+      trades["base_asset_issuer": "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN",
+      trades["counter_offer_id": "997080830",
+      trades["counter_account": "GBEZLZV7VUKAPQ5LME2SIGG5ER5RH5P22SRF5OP3HQCY6H6V6GCHISIN",
+      trades["counter_amount": "129.9698449",
+      trades["counter_asset_type": "credit_alphanum12",
+      trades["counter_asset_code": "yUSDC",
+      trades["counter_asset_issuer": "GDGTVWSM4MGS4T7Z6W4RPWOCHE2I6RDFCIFZGS3DOA63LWQTRNZNTTFF",
+      : false,
+      if():
+        i am counter
+      else:
+        i am base
+      
+      
+      # extract asset
+      # part of averaged single sale? -> begin averaging
+        # sum all bought / sum all given etc
+      # extract value at txn for own benefit
+      
+      # a way to work cost basis into this ?
+      
+      
+      
+      
+      # BASICALLY:
+      # Can create account record for uncovered positions or pre-existing basis totally averaged out mst likely
+      # When they go to sell, they pick the lot
+      # Lots classified by OFFER_ID and averages as needed when providing liquidity
+      # When selling (from account omnibus), Use MEMO "Disposing #{OFFER_ID}"
+      
+      
+      
+      
+      bis = trades["trade_type"]
+      if(bis != "orderbook"):
+        pprint(trades)
       else:
         b += 1
       #for balances in accounts["balances"]:
@@ -48,10 +86,7 @@ def getAllTxnsFromLastYear():
     data = requests.get(requestAddr).json()
     blockchainRecords = data["_embedded"]["records"]
   
-  
-  print(a)
-  print(b)
-  #- cycle through txns using taxYearStart to 
+    #- cycle through txns using taxYearStart to 
   
   
   
