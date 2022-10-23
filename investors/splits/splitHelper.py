83698bce18b1a19646a73c0b0206434bcc77d92a
John Wooten <jwooten31@gatech.edu>
2022-10-23 18:50:49 -0400

    Update splitHelper.py

--- investors/splits/splitHelper.py
+++ investors/splits/splitHelper.py
@@ -2,6 +2,27 @@ import sys
 sys.path.append("../../")
 from globals import *
 
+def getClaimableBalancesData(queryAsset):
+  claimableBalanceIDsMappedToData = data = {}
+  b = []
+  requestAddr = f"{HORIZON_INST}/claimable_balances?asset={queryAsset}:{BT_ISSUER}&{MAX_SEARCH}"
+  ledger = requests.get(requestAddr).json()
+  while(ledger["_embedded"]["records"]):
+    for claimableBalances in ledger["_embedded"]["records"]:
+      b.append(str(claimableBalances))
+      data["release"] = 0
+      for claimants in claimableBalances["claimants"]:
+        try:
+          data["release"] = claimants["predicate"]["not"]["abs_before_epoch"]
+        except KeyError:
+          continue # Expect investor as claimant via not abs_before
+      if(data["release"]):
+        data["recipient"] = claimants["destination"]
+        data["amount"] = Decimal(claimableBalances["amount"])
+        claimableBalanceIDsMappedToData[claimableBalances["id"]] = data
+    ledger = getNextLedgerData(ledger)
+  return claimableBalanceIDsMappedToData, b
+
 def prep(transaction, reason):
   return transaction.add_text_memo(reason).set_timeout(7200).build()
 
@@ -40,25 +61,4 @@ def exportSplitNewShareTransactions(txnArr, queryAsset):
     output = open(f"{str(datetime.now()).replace(':','.')} {queryAsset} StockSplitOutputXDR.txt", "w")
     output.write(txns.to_xdr())
     output.close()
-
-def getClaimableBalancesData(queryAsset):
-  claimableBalanceIDsMappedToData = data = {}
-  b = []
-  requestAddr = f"{HORIZON_INST}/claimable_balances?asset={queryAsset}:{BT_ISSUER}&{MAX_SEARCH}"
-  ledger = requests.get(requestAddr).json()
-  while(ledger["_embedded"]["records"]):
-    for claimableBalances in ledger["_embedded"]["records"]:
-      b.append(str(claimableBalances))
-      data["release"] = 0
-      for claimants in claimableBalances["claimants"]:
-        try:
-          data["release"] = claimants["predicate"]["not"]["abs_before_epoch"]
-        except KeyError:
-          continue # Expect investor as claimant via not abs_before
-      if(data["release"]):
-        data["recipient"] = claimants["destination"]
-        data["amount"] = Decimal(claimableBalances["amount"])
-        claimableBalanceIDsMappedToData[claimableBalances["id"]] = data
-    ledger = getNextLedgerData(ledger)
-  return claimableBalanceIDsMappedToData, b
 
