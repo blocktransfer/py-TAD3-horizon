f30478e43c0154907fef076a08e5646a7dad1e62
John Wooten <jwooten31@gatech.edu>
2023-06-06 13:34:36 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/blocktransfer/stellar-interface""
    This reverts commit 80626b1b30ab283d75151617b0f89e783e888b0f.

--- globalToolsSearching.py
+++ globalToolsSearching.py
@@ -1,8 +1,27 @@
 from globals import *
 
+def getAssetIssuer(queryAsset):
+  requestAddr = f"{HORIZON_INST}/assets?asset_code={queryAsset}&asset_issuer="
+  for addresses in BT_ISSUERS:
+    if(requests.get(requestAddr + addresses).json()["_embedded"]["records"]):
+      return addresses
+  sys.exit(f"Could not find asset {queryAsset}")
+
+def getAssetAddress(queryAsset):
+  issuer = getAssetIssuer(queryAsset)
+  return f"{HORIZON_INST}/assets?asset_code={queryAsset}&asset_issuer={issuer}"
+
+def getAssetAccountsAddress(queryAsset):
+  issuer = getAssetIssuer(queryAsset)
+  return f"{HORIZON_INST}/accounts?asset={queryAsset}:{issuer}&{MAX_SEARCH}"
+
+def getIssuerAccObj(queryAsset):
+  issuer = getAssetIssuer(queryAsset)
+  return server.load_account(account_id = issuer)
+
 def getNumRestrictedShares(queryAsset):
-  assetAddr = f"{HORIZON_INST}/assets?asset_code={queryAsset}&asset_issuer={BT_ISSUER}"
-  assetData = requests.get(assetAddr).json()["_embedded"]["records"][0]
+  requestAddr = getAssetAddress(queryAsset)
+  assetData = requests.get(requestAddr).json()["_embedded"]["records"][0]
   explicitRestrictedShares = Decimal(assetData["claimable_balances_amount"])
   implicitRestrictedShares = Decimal("0")
   for classifiers, balances in assetData["balances"].items():  
@@ -24,26 +43,25 @@ def getFederationServerFromDomain(federationDomain):
   except requests.exceptions.ConnectionError:
     return ""
 
-def resolveFederationAddress(queryAddr):
-  splitAddr = queryAddr.split("*")
-  federationName = splitAddr[0]
-  federationDomain = splitAddr[1]
+def resolveFederationAddress(federationAddress):
+  federationDomain = federationAddress.split("*")[1]
   homeDomainFederationServer = getFederationServerFromDomain(federationDomain)
-  requestAddr = f"{homeDomainFederationServer}?q={queryAddr}&type=name"
+  requestAddr = f"{homeDomainFederationServer}?q={federationAddress}&type=name"
   try:
     return requests.get(requestAddr).json()["account_id"]
   except requests.exceptions.MissingSchema:
     return ""
 
-def getNumAuthorizedSharesNotIssued(companyCode):
-  issuerAccounts = [
+# todo: Change diction to reflect use of Soroban for options compensation data
+def getNumAuthorizedSharesNotIssued(companyCode): # todo: Change this diction to reserved shares?
+  issuerAccounts = [ # todo: Change this diction to companyAccounts?
     "authorized.DSPP",
     "initial.offering",
     "reg.a.offering",
     "reg.cf.offering",
     "reg.d.offering",
     "shelf.offering",
-    "reserved.employee", # todo: stock options via Soroban
+    "reserved.employee", # todo: stock options via Soroban -> these held in contract
     "treasury"
   ]
   shares = Decimal("0")
@@ -57,15 +75,17 @@ def getCustodiedShares(queryAsset, account):
   requestAddr = f"{HORIZON_INST}/accounts/{account}"
   accountBalances = requests.get(requestAddr).json()["balances"]
   asset = getAssetObjFromCode(queryAsset)
+  ### todo: Globalize this? ###
   for balances in accountBalances:
     searchAsset = Asset(balances["asset_code"], balances["asset_issuer"])
     if(balances["asset_type"] != "native" and searchAsset == asset):
       return balances["balance"]
+  ######
 
-def getAffiliateShares(queryAsset):
+def getAffiliateShares(queryAsset): # TODO: rm, outdated
   companyCode = getCompanyCodeFromAssetCode(queryAsset)
-  type = getCompanyType(companyCode)
-  if(type == "private"):
+  public = isPublic(companyCode)
+  if(not public):
     affiliateAccount = requestAccount = resolveFederationAddress(f"{companyCode}*private.affiliate.holdings")
     return getCustodiedShares(queryAsset, affiliateAccount)
   else:
@@ -80,8 +100,12 @@ def getCompanyCodeFromAssetCode(queryAsset):
   for assets in loadTomlData(BT_STELLAR_TOML)["CURRENCIES"]:
     if(assets["code"] == queryAsset):
       issuerInfo = assets["attestation_of_reserve"]
-      return loadTomlData(issuerInfo)["bt_company_code"]
+      return loadTomlData(issuerInfo)["ISSUER"]["bt_company_code"]
   return 0
+
+def isPublic(companyCode):
+  issuerInfo = f"https://blocktransfer.io/assets/{companyCode}.toml"
+  return loadTomlData(issuerInfo)["ISSUER"]["reporting_company"]
 
 def getNumTreasuryShares(queryAsset):
   treasuryAddr = resolveFederationAddress(f"{queryAsset}*treasury.holdings")
@@ -141,14 +165,44 @@ def getOfferIDsMappedToChiefMemosFromCache():
     try:
       offerID = int(offerIDs)
     except ValueError:
-      sys.exit("Critical data validity error")
+      sys.exit("Bad validity: Searching/offer-memos")
     offerIDsMappedToChiefMemos[offerID] = memos
   return offerIDsMappedToChiefMemos
 
-def getWashSaleOfferIDsMappedToAdjustments(combinedTradeData):
+def getWashSaleOfferIDsMappedToAdjustments():
   washSaleOfferIDsMappedToAdjustments = {}
-  cache = loadTomlData(WASH_SALES_TOML)
+  cache = loadTomlData(WASH_SALE_TOML)
   for offerIDs, adjustments in cache.items():
-    offerIDsMappedToChiefMemos[offerIDs] = adjustments
+    washSaleOfferIDsMappedToAdjustments[offerIDs] = adjustments
   return washSaleOfferIDsMappedToAdjustments
+
+def getMemoFromTransaction(txn):
+  try:
+    return txn["memo"]
+  except KeyError:
+    return ""
+
+def getCBmemoFromClaimableID(ID):
+  CBcreationTxn = getCBcreationTxnFromClaimableID(ID)
+  return getMemoFromTransaction(CBcreationTxn)
+
+def getCBcreationTxnFromClaimableID(ID):
+  requestAddr = f"{HORIZON_INST}/claimable_balances/{ID}/transactions"
+  return requests.get(requestAddr).json()["_embedded"]["records"][0]
+
+def getClaimedIDfromClaimingTxnHashForAsset(transaction, queryAsset):
+  requestAddr = f"{HORIZON_INST}/transactions/{transaction}/operations?limit={MAX_NUM_TXN_OPS}"
+  userClaimTxnOps = requests.get(requestAddr).json()["_embedded"]["records"]
+  for ops in userClaimTxnOps:
+    try:
+      originClaimableID = ops["balance_id"]
+    except KeyError:
+      continue
+    claimingOpEffects = requests.get(ops["_links"]["effects"]["href"])
+    for effects in claimingOpEffects.json()["_embedded"]["records"]:
+      try:
+        if(effects["asset"].split(":")[0] == queryAsset):
+          return originClaimableID
+      except KeyError:
+        continue
 
